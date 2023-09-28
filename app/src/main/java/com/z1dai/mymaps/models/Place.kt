b7cd37c342236172ba4b556def7bef1b9715d5e5
Imss27 <1529767689@qq.com>
2023-09-27 22:43:37 -0700

    Layout completed.

---
+++ app/src/main/java/com/z1dai/mymaps/models/Place.kt
@@ -0,0 +1,3 @@
+package com.z1dai.mymaps.models
+
+data class Place(val title: String, val description: String, val latitude: Double, val longitude: Double)
---
+++ app/src/main/java/com/z1dai/mymaps/models/UserMap.kt
@@ -0,0 +1,3 @@
+package com.z1dai.mymaps.models
+
+data class UserMap(val title: String, val places: List<Place>)
